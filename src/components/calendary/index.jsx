6ec523f8af51a8bb414730dc6386eef3c9d1fa64
Miguel Enrique Saca Accostupa <miguelenrique1805@gmail.com>
2022-07-08 18:31:24 -0500

    add calendary part 3

--- src/components/calendary/index.jsx
+++ src/components/calendary/index.jsx
@@ -3,15 +3,40 @@ import Arrows from "./components/arrows";
 import Years from "./components/years";
 import "./styles.scss";
 
-const Calendary = ({ text }) => {
+const Calendary = () => {
   const slide = useRef(null);
 
   const handleClickRight = () => {
-    console.log(slide.current);
+    if (slide.current.children.length > 0) {
+      const firstElement = slide.current.children[0];
+      slide.current.style.transition = `${200}ms ease-out all`;
+      slide.current.style.transform = `translateY(-${160}px)`;
+      const transition = () => {
+        slide.current.style.transition = "none";
+        slide.current.style.transform = `translateX(0)`;
+        slide.current.appendChild(firstElement);
+        slide.current.removeEventListener("transitionend", transition);
+      };
+      slide.current.addEventListener("transitionend", transition);
+    }
   };
+
   const handleClickLeft = () => {
-    console.log("anterior");
+    if (slide.current.children.length > 0) {
+      const index = slide.current.children.length - 1;
+      const lastElement = slide.current.children[index];
+      slide.current.insertBefore(lastElement, slide.current.firstChild);
+
+      slide.current.style.transition = "none";
+      slide.current.style.transform = `translateY(-${160}px)`;
+
+      setTimeout(() => {
+        slide.current.style.transition = `${200}ms ease-out all`;
+        slide.current.style.transform = `translateY(0)`;
+      }, 30);
+    }
   };
+  
   return (
     <div className="calendary">
       <div className="calendary__arrows">
@@ -21,7 +46,9 @@ const Calendary = ({ text }) => {
           text={"2005"}
         />
       </div>
-      <Years refe={slide} />
+      <div className="calendary__list">
+        <Years refe={slide} />
+      </div>
     </div>
   );
 };
