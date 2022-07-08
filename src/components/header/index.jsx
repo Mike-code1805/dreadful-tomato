435f14f0732d03222271bf807c2c7f50b3f1be7e
Miguel Enrique Saca Accostupa <miguelenrique1805@gmail.com>
2022-07-07 22:23:58 -0500

    add header

---
+++ src/components/header/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./styles.scss";
+
+
+const Header = () => {
+  return (
+    <div className="header">
+        HEADER
+    </div>
+  )
+}
+
+export default Header
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Footer from "../components/footer";
+import Header from "../components/header";
 
 const HomePage = () => {
   return (
     <div>
+      <Header />
       <Footer />
     </div>
   );
